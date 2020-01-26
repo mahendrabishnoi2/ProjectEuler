b78a174f7a01428f430dfc8b06a86d4b195d1bd2
Mahendra Bishnoi <mahendrabishnoi2@gmail.com>
2020-01-26 14:15:14 +0530

    Project Euler #6 done

---
+++ Solution006.java
@@ -0,0 +1,22 @@
+ /*
+https://www.hackerrank.com/contests/projecteuler/challenges/euler005/problem
+Project Euler #6: Sum square difference
+*/
+import java.io.*;
+import java.util.*;
+
+public class Solution006 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            long n = in.nextLong();
+            long sum_n = (n * (n+1)) / 2;
+            long sum_n_sqr = sum_n * sum_n;
+
+            long sqr_n_sum = (n * (n+1) * (2*n + 1)) / 6;
+            System.out.println(sum_n_sqr - sqr_n_sum);
+        }
+    }
+}
