2a43ca33c6ed1990a3afeb531d6ddd2175b877ed
Mahendra Bishnoi <mahendrabishnoi2@gmail.com>
2020-02-29 00:05:54 +0530

    #13 complete using BigInteger

---
+++ Solution013a.java
@@ -0,0 +1,28 @@
+/*
+https://www.hackerrank.com/contests/projecteuler/challenges/euler013/problem
+Project Euler #13: Large sum
+*/
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+
+public class Solution {
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int t = in.nextInt();
+    
+    BigInteger sum;
+    sum = BigInteger.ZERO;
+
+    for (int i = 0; i < t; i++) {
+      sum = sum.add(in.nextBigInteger());
+    }
+    
+    String sum_out = sum.toString();
+    for (int i = 0; i < 10; i++) {
+      System.out.print(sum_out.charAt(i));
+    }
+    System.out.println();
+  }
+}
